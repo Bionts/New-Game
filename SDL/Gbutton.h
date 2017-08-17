178737941c6e6d271810cd02714b85485b4c338b
katcky <1246454212@qq.com>
2017-08-16 22:09:39 -0500

    Update Gbutton.h

--- SDL/Gbutton.h
+++ SDL/Gbutton.h
@@ -1,12 +1,21 @@
-/static bool pressbutton=false;
+static bool pressbutton=false;
 static int buttonID=0;
 ///////////////////////////////////////////////
+/*文件使用说明：这是一个按钮类，只有结构，通过
+Gbutton name("标签题目",SDL_Rect,*可见性false或true*,*按钮所在页数*,*按钮ID号*);
+来创建，
+int temp=name.GButtonb(*鼠标指针位置需要用SDL_Rect的x，y来记录*);
+于是temp就得到了按钮的ID；
 
 
 
+
+
+*/
     /////////////////////////////////////////////////////
     class Gbutton{
     public:
+    
     Gbutton(const std::string,SDL_Rect,bool,int,int);
     bool GbuttonSee;
     int Gbuttonb(SDL_Rect);
