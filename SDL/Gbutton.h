cc44353db381119ace6f685082a29336f2ada6dd
katcky <1246454212@qq.com>
2017-08-16 22:21:27 -0500

    Update Gbutton.h

--- SDL/Gbutton.h
+++ SDL/Gbutton.h
@@ -5,7 +5,7 @@ static int buttonID=0;
 Gbutton name("标签题目",SDL_Rect,*可见性false或true*,*按钮所在页数*,*按钮ID号*);
 来创建，
 int temp=name.GButtonb(*鼠标指针位置需要用SDL_Rect的x，y来记录*);
-于是temp就得到了按钮的ID；
+于是temp的返回数值判断是否获得ID，全局变量buttonID就得到了按钮的ID；
 
 
 
@@ -35,7 +35,10 @@ int temp=name.GButtonb(*鼠标指针位置需要用SDL_Rect的x，y来记录*);
 
     }
     int Gbutton::Gbuttonb(SDL_Rect GbuttonRectb){
-    if(buttonID!=0){
+   
+        //如果buttonID有ID了，其它的按钮判断指针位置的赋值全部失败，不会因为判断失败把buttonID的值清零；适用于把所有按钮全部加入一个循环匹配事件中。
+        
+        if(buttonID!=0){
     int temp =buttonID;
     return temp;};
     if(this->GbuttonSee==false)return 0;
@@ -46,7 +49,8 @@ int temp=name.GButtonb(*鼠标指针位置需要用SDL_Rect的x，y来记录*);
         if(GbuttonRectb.y>=this->GbuttonRect.y&&GbuttonRectb.y<=this->GbuttonRect.h)
             {
 
-             return this->GbuttonID;
+             buttonID=this->GbuttonID;
+            return 1;
 
 
             };
